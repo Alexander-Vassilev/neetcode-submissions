75113da9ee5821c8f45aae437fb95bfa86060cb8
Alexander-Vassilev <alex.s.vassilev@gmail.com>
2026-08-11 17:15:08 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+
+            cout << "left: " << left << endl;
+            cout << "right: " << right << endl;
+            cout << "mid: " << mid << endl;
+            cout << endl;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
