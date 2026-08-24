781ab9eaf4f0d3e763bb9b1b4443b5ed77a9765b
Alexander-Vassilev <alex.s.vassilev@gmail.com>
2026-08-23 17:33:30 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int len = nums.size();
+        nums[len - 1] = 0;
+
+        for (int i = len - 2; i >= 0; i--) {
+            int minJumps = 1e9;
+            int itLimit = min(i + nums[i] + 1, len);
+
+            for (int j = i + 1; j < itLimit; j++) {
+                minJumps = min(minJumps, nums[j]);
+            }
+
+            nums[i] = minJumps + 1;
+            //cout << minJumps << endl;
+        }
+
+        return nums[0];
+    }
+};
