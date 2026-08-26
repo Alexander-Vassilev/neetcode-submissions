9e30f7508a22ca9d22fe86645ed8100a2fcc74e9
Alexander-Vassilev <alex.s.vassilev@gmail.com>
2026-08-26 13:12:41 -0700

    Add: multiply-strings - submission-3

---
+++ Data Structures & Algorithms/multiply-strings/submission-3.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int num1Len = num1.size();
+        int num2Len = num2.size();
+        int totLen = num1Len + num2Len;
+        vector<uint8_t> a(totLen, 0);
+        vector<uint8_t> b(totLen, 0);
+        int iFlip = 0;
+        
+        for (int i = num1Len - 1; i >= 0; i--) {
+            int val1 = num1[i] - '0';
+            int jFlip = 0;
+            uint8_t carry = 0;
+
+            for (int j = num2Len - 1; j >= 0; j--) {
+                int val2 = num2[j] - '0';    
+                int temp = val1 * val2 + carry;
+                uint8_t ones = temp % 10;
+                int tens = (temp - ones) / 10;
+                carry = tens;
+
+                a[totLen - 1 - jFlip - iFlip] = ones;
+                jFlip++;
+            }
+
+            a[totLen - 1 - jFlip - iFlip] = carry;
+            carry = 0;
+
+            for (int j = totLen - 1; j >= 0; j--) {
+                int temp = b[j] + a[j] + carry;
+                uint8_t ones = temp;
+                carry = 0;
+
+                if (temp >= 10) {
+                    carry = 1;
+                    ones -= 10;
+                }
+
+                b[j] = ones;
+            }
+
+            fill(a.begin(), a.end(), 0);
+            iFlip++;
+        }
+
+        string ret = "";
+        bool started = false;
+
+        for (int i = 0; i < totLen; i++) {
+            if (b[i] != 0) started = true;
+
+            if (started) {
+                char c = b[i] + '0';
+                ret += c;
+            }
+        }
+
+        if (ret.size() == 0) return "0";
+        return ret;
+    }
+};
